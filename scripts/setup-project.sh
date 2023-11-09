a12882996f5d9a27df0449fb670a9441de123072
Nikola Markovic <nikola.m@gmail.com>
2023-11-09 13:02:27 -0600

    setup-project.sh referencing a new commit for the parent baseline

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -18,7 +18,7 @@ wsdir=./stm32
 rm -rf aws-stm32-ml-at-edge-accelerator
 git clone https://github.com/aws-samples/aws-stm32-ml-at-edge-accelerator
 pushd aws-stm32-ml-at-edge-accelerator >/dev/null
-  git reset --hard e17843d19ba0610da596cfc1cea90d6cbbaf94f9
+  git reset --hard b8271253a2d811a6db184fef35fcdd94d505b848
 popd >/dev/null
 
 cp -rn aws-stm32-ml-at-edge-accelerator/stm32/* ./stm32/
@@ -26,8 +26,11 @@ rm -rf aws-stm32-ml-at-edge-accelerator
 
 mkdir -p ${wsdir}/Middleware/STM32_AI_Library/Inc
 mkdir -p ${wsdir}/Middleware/STM32_AI_Library/Lib
+
+echo Mpplying the model at ${mlpath}...
 cp -rf ${mlpath}/stm32ai_files/Inc/* ${wsdir}/Middleware/STM32_AI_Library/Inc/
 cp -f ${mlpath}/stm32ai_files/Lib/NetworkRuntime730_CM33_GCC.a ${wsdir}/Middleware/STM32_AI_Library/Lib/NetworkRuntime800_CM33_GCC.a
 cp -rf ${mlpath}/C_header/* ${wsdir}/Projects/Common/dpu/
 cp -rf ${mlpath}/stm32ai_files/network*.c ${wsdir}/Projects/Common/X-CUBE-AI/App/
 cp -rf ${mlpath}/stm32ai_files/network*.h ${wsdir}/Projects/Common/X-CUBE-AI/App/
+echo Done.
